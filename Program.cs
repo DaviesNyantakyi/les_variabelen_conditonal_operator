1aa4cf4ef0de84cf9464b914de85317d170eddc8
DaviesNyantakyi <davies.nyantakyi@outlook.com>
2024-08-03 01:47:30 +0200

    les conditional operator

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace BerekeningTarief
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            int leeftijd;
+            bool aanmerking;
+
+
+            Console.Write($"Geef je leeftijd: ");
+            leeftijd = int.Parse(Console.ReadLine());
+
+            // Leeftijd moet kleiner of groter zijn dan 65+ om toegelaten te worden
+            aanmerking = leeftijd <= 18 || leeftijd >= 65;
+            Console.WriteLine($"Korting door leeftijd < 18 of > 65: {aanmerking}");
+        }
+    }
+}
+
+
+
